8b531692267ec454d652f4583a9a70f063bdf4c1
RLCHuncp <rlch06.uncp@gmail.com>
2020-09-19 00:45:23 -0500

    Creando archivo de rutas

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+    {
+        path: '/',
+        template: <h1>Hola</h1>
+    },
+    {
+        path: '/contacto',
+        template: <h1>Contacto</h1>
+    },
+    {
+        path: '/nosotros',
+        template: <h1>Nosotros</h1>
+    }
+];
